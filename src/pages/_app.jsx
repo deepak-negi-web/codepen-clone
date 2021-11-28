d782d38f4ed0a7746b3c01e2a6fe613d9dbffda3
Deepak Negi <st.deepak15@gmail.com>
2021-11-29 00:54:33 +0530

    fix: google font import not working

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 import "antd/dist/antd.css";
 import "tailwindcss/tailwind.css";
-import "../styles/globals.css";
 import { GlobalStyles } from "twin.macro";
+import "../styles/globals.css";
 import { Header, Footer, ModalManager } from "../components";
 import { ApolloProvider, ModalProvider } from "../providers";
 function MyApp({ Component, pageProps }) {
