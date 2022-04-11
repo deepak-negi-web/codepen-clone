20f30d38d919820dca4b6034d94d4dc7995d0bbb
Deepak Negi <st.deepak15@gmail.com>
2022-04-11 23:06:00 +0530

    refactor:reduce lines from htmlToImage api

--- src/pages/api/htmlToImage.js
+++ src/pages/api/htmlToImage.js
@@ -1,54 +1,29 @@
-import chromium from "chrome-aws-lambda";
-import puppeteer from "puppeteer";
-async function getBrowserInstance() {
-  const executablePath = await chromium.executablePath;
-  if (!executablePath) {
-    // running locally
-    return puppeteer.launch({
-      args: chromium.args,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    });
-  }
-  await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
-}
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { html } = req.body;
-    let imageBuffer = null;
-    let browser = null;
-    try {
-      if (!html || !html.trim()) {
-        res.json({
-          status: "error",
-          message: "No html provided",
-        });
-        return;
+export default async function screenshot(req, res) {
+  const { html } = req.body;
+  const options = process.env.AWS_REGION
+    ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
       }
-      browser = await getBrowserInstance();
-      const page = await browser.newPage();
-      await page.setContent(html);
-      const content = await page.$("body");
-      imageBuffer = await content.screenshot({
-        omitBackground: true,
-      });
-      await page.close();
-      await browser.close();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      if (browser !== null) {
-        await browser.close();
-      }
-    }
-    res.json({ imageBuffer, imageBase64: imageBuffer.toString("base64") });
-  }
-  return res.json({ status: "error", message: "Invalid request" });
+    : {
+        args: [],
+        executablePath:
+          process.platform === "win32"
+            ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+            : process.platform === "linux"
+            ? "/usr/bin/google-chrome"
+            : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      };
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+  await page.setContent(html);
+  const content = await page.$("body");
+  const imageBuffer = await content.screenshot({
+    omitBackground: true,
+  });
+  res.json({ imageBuffer, imageBase64: imageBuffer.toString("base64") });
 }
